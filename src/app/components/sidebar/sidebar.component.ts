688801ecf53101c71e28d6c3e1c5d685c892b915
xenxei46 <wisdomjohnikoi@gmail.com>
2023-10-23 21:53:41 +0100

    implementing side-bar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -27,10 +27,13 @@ export class SidebarComponent implements OnInit {
 
   ROUTES: RouteInfo[] = [
     // { path: '/dashboard', title: 'Dashboard', icon: 'ni ni-shop', class: '', children: '' },
+
+    
   
     {
       id:0,
-      path: '/admin', title: 'Admin', icon: 'fa-solid fa-user-tie', class: '', children: [
+      path: '/admin', title: 'Admin', icon: 'fa-solid fa-user-tie', class: '', 
+      children: [
         { path: '/admin/add-admin', title: 'Add Admin', icon: 'fa fa-user-plus', class: '' },
         { path: '/admin/view-admin', title: 'View Admin', icon: 'fa fa-eye', class: '' }
       ]
@@ -155,6 +158,17 @@ export class SidebarComponent implements OnInit {
   
   ];
 
+  carret_down =  false;
+  carret_up = true;
+
+  toggleCarretDown() {
+    this.carret_down = true;
+    this.carret_up = false;
+  }
+  toggleCarretUp() {
+    this.carret_down = false;
+    this.carret_up = true;
+  }
 
 
   // user_type:any;
