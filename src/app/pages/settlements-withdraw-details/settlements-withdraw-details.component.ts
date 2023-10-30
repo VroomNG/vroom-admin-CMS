b431de34f8b4c39315c27ec3b09fd1562b5e3144
xenxei46 <wisdomjohnikoi@gmail.com>
2023-10-30 12:59:50 +0100

    settlements

--- src/app/pages/settlements-withdraw-details/settlements-withdraw-details.component.ts
+++ src/app/pages/settlements-withdraw-details/settlements-withdraw-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Iwithdraw_Request } from 'src/app/model/settlements';
 
 @Component({
   selector: 'app-settlements-withdraw-details',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./settlements-withdraw-details.component.scss']
 })
 export class SettlementsWithdrawDetailsComponent {
-
+  withdraw: Iwithdraw_Request [] = [
+    {
+      id: 1,
+      driver_code:'DRV00755',
+      driver_name: 'Moshood Salu',
+      driver_email: 'tpumpingt@gmail.com',
+      phone: '+2347031582774',
+      withdrawn_amount: '2800',
+      withdraw_date: '2023-10-25T00:28:28.000Z',
+      status: '',
+      details_history: true,
+    },
+    {
+      id: 1,
+      driver_code:'DRV00755',
+      driver_name: 'Moshood Salu',
+      driver_email: 'tpumpingt@gmail.com',
+      phone: '+2347031582774',
+      withdrawn_amount: '2800',
+      withdraw_date: '2023-10-25T00:28:28.000Z',
+      status: '',
+      details_history: true,
+    },
+    {
+      id: 1,
+      driver_code:'DRV00755',
+      driver_name: 'Moshood Salu',
+      driver_email: 'tpumpingt@gmail.com',
+      phone: '+2347031582774',
+      withdrawn_amount: '2800',
+      withdraw_date: '2023-10-25T00:28:28.000Z',
+      status: '',
+      details_history: true,
+    },
+    {
+      id: 1,
+      driver_code:'DRV00755',
+      driver_name: 'Moshood Salu',
+      driver_email: 'tpumpingt@gmail.com',
+      phone: '+2347031582774',
+      withdrawn_amount: '2800',
+      withdraw_date: '2023-10-25T00:28:28.000Z',
+      status: '',
+      details_history: true,
+    },
+    {
+      id: 1,
+      driver_code:'DRV00755',
+      driver_name: 'Moshood Salu',
+      driver_email: 'tpumpingt@gmail.com',
+      phone: '+2347031582774',
+      withdrawn_amount: '2800',
+      withdraw_date: '2023-10-25T00:28:28.000Z',
+      status: '',
+      details_history: true,
+    }
+  
+    ];
+  
 }
