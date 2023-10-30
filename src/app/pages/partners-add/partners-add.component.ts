3897c207cfd8dd8e03f4766859e91e8562c8803e
xenxei46 <wisdomjohnikoi@gmail.com>
2023-10-30 13:05:40 +0100

    partners

--- src/app/pages/partners-add/partners-add.component.ts
+++ src/app/pages/partners-add/partners-add.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup,Validators, } from '@angular/forms';
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-partners-add',
@@ -7,4 +12,29 @@ import { Component } from '@angular/core';
 })
 export class PartnersAddComponent {
 
+  adminForm!: FormGroup
+  cities!: City[] |  undefined;
+
+  constructor(private formBuilder: FormBuilder){
+    this.adminForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['',[Validators.required, Validators.email]]
+
+    })
+  }
+
+  onSubmit(){
+    if(this.adminForm.valid) {
+     window.alert('admin added')
+    }
+  }
+  ngOnInit(){
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+  ];
+  }
 }
