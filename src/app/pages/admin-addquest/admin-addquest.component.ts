41485feb6a663403b1d1d3379fc40c38ed3cea40
codeSage-X <wisdomjohnikoi@gmail.com>
2024-05-27 17:32:41 +0100

    live server set

--- src/app/pages/admin-addquest/admin-addquest.component.ts
+++ src/app/pages/admin-addquest/admin-addquest.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { RegisterValidators } from '../../helpers/validators/register-validators'
 // import { EmailTaken } from '../../helpers/validators/email-taken';
 import { AdminService } from 'src/app/service/admin.service';
@@ -13,6 +13,10 @@ interface Users {
   type: string;
   code: string;
 }
+interface Icon {
+  icon: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-admin-addquest',
@@ -23,9 +27,35 @@ interface Users {
 export class AdminAddQuestComponent implements OnInit {
 
   inSubmission = false; 
+  data = [
+    {
+      "title": "Embark on an adventurous journey to explore the urban wonders around you.",
+      "list": [
+        {
+          "icon": "bullet",
+          "content": "Discover and visit five unique landmarks in the city."
+        },
+        {
+          "icon": "bullet",
+          "content": "Capture a photo at each landmark to document your exploration."
+        }
+      ]
+    },
+    {
+      "title": "Reward Showcase",
+      "list": [
+        {
+          "icon": "https://image-url.com/star.jpg",
+          "title": "Explorer Badge",
+          "content": "Discover and visit five unique landmarks in the city."
+        }
+      ]
+    }
+  ];
 
   cities!: City[] |  undefined;
   userType!: Users[] |  undefined;
+  iconType!: Icon[] |  undefined;
   userDetails:any
   
   showAlert = false;
@@ -34,93 +64,91 @@ export class AdminAddQuestComponent implements OnInit {
 
   constructor(
     private users: UsersService,
-     private Admin: AdminService
+    private Admin: AdminService,
+    private fb: FormBuilder
     ){ }
 
-  onSubmit(){
-    if(this.questForm.valid) {
-     window.alert('admin added')
-    }
-  }
+ 
   ngOnInit(){
     this.userType = [
       { type: 'Badge', code: '1' },
       { type: 'Coin', code: '2' },
   ];
  
-  // const userDetails = this.users.getStoredUserDetails();
-  // this.userDetails = userDetails
-  // this.addAccessTrail()
-  console.log('init')
+    this.iconType = [
+      { icon: 'Bullet', code: '1' },
+      { icon: 'Icon', code: '2' },
+  ];
+ 
+  console.log('init');
+
   }
 
   title = new FormControl('',[Validators.required, Validators.minLength(3)])
   subTitle = new FormControl('',[Validators.required, Validators.minLength(3)])
-  description  = new FormControl('',[Validators.required, Validators.minLength(3)])
+  // description  = new FormArray([],[Validators.required, Validators.minLength(3)])
   icon = new FormControl(<any>[Validators.required, Validators.minLength(3)]);
   type = new FormControl('',[Validators.required, Validators.minLength(3)])
   points = new FormControl('',[Validators.required, Validators.minLength(1)])
   target = new FormControl('',[Validators.required, Validators.minLength(1)])
   starts = new FormControl('',[Validators.required, Validators.minLength(3)])
   expires = new FormControl('',[Validators.required, Validators.minLength(3)])
- 
-  
-  questForm = new FormGroup({
-    title: this.title,
-    subTitle: this.subTitle,
-    description: this.description,
-    icon: this.icon,
-    type: this.type,
-    points: this.points,
-    target: this.target,
-    starts: this.starts,
-    expires: this.expires
+  content = new FormControl('',[Validators.required, Validators.minLength(3)])
+  title_ = new FormControl('',[Validators.required, Validators.minLength(3)])
+
+  descForm = new FormGroup({
+      description: this.fb.array([]),
   })
 
-  // addAccessTrail(){
-  //   const {email} = this.userDetails
-  //   const userCredetials = {
-  //     login: email,
-  //     action: 'Viewed Add Quests'
-  //   }
+  credentials = {
+    title:'',
+    list:  [
+      {
 
-  //   this.users.addAccesstrail(userCredetials).subscribe(
-  //     (res:any)=>{
-  //       const {message} = res
-  //       if(message === "Success insering access"){
-  //        } else {
-  //        }
-  //     }
-  //   )
-  // }
+      }
+    ]
+  }
+
+  questForm = new FormGroup({
+    title: this.title,
+      subTitle: this.subTitle,
+      description: this.fb.array([]),
+      icon: this.icon,
+      type: this.type,
+      points: this.points,
+      target: this.target,
+      starts: this.starts,
+      expires: this.expires
+  })
+
+  get description(){
+    return this.descForm.controls["description"] as FormArray
+  }
+
+  addDesc(){
+    const descriptionForm = this.fb.group({
+      title: "",
+      // level: ['beginner', Validators.required]
+      list: [
+        {
+          icon: "bullet",
+          content: " "
+        },
+      ]
+    })
+
+    this.description.push(descriptionForm);
+  }
+
+  delete(questIndex:number){
+      this.description.removeAt(questIndex);
+  }
 
   submit(){
-     console.log(this.questForm.value)
-
-    this.showAlert = true
-    setTimeout(() => {
-      this.showAlert = true
-      this.alertMsg = 'Adding Quest'
-      this.alertColor = 'info'
-      this.Admin.addQuest(this.questForm.value).subscribe(
-        (res:any) => {
-         console.log(res)
-         if(res.code == 200){
-          this.alertMsg = 'Quest Created Successfully';
-          this.alertColor = "success"
-         } else {
-          this.alertMsg = 'something went wrong check connectivity and try again';
-          this.alertColor = 'danger'
-         }
-         }
-        
-      )
-      }, 1600)
+    console.log(this.descForm.value)
   }
 
-  reset(){
-    this.questForm.reset()
-  }
+ 
 
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
